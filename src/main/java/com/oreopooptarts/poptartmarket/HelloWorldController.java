b7e1897a49e7cba6cf76b3c696c0690745a86a56
Marcos <magma_ruby145@hotmail.com>
2023-07-08 15:15:31 -0700

    first commit

---
+++ src/main/java/com/oreopooptarts/poptartmarket/HelloWorldController.java
@@ -0,0 +1,4 @@
+package com.oreopooptarts.poptartmarket;
+
+public class HelloWorldController {
+}
---
+++ src/main/java/com/oreopooptarts/poptartmarket/PoptartMarketApplication.java
@@ -0,0 +1,13 @@
+package com.oreopooptarts.poptartmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // indica a spring que esta es la clase que debe de ejecutar
+public class PoptartMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoptartMarketApplication.class, args);
+	}
+
+}
